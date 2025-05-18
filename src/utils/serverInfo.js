6ab35554ad872bdb43ad19be3faf9baa770b4389
yarik.s <yarik.savelev.04@mail.ru>
2025-05-18 13:44:10 +0300

    serverinfo

--- src/utils/serverInfo.js
+++ src/utils/serverInfo.js
@@ -13,26 +13,35 @@ function getLocalIP() {
   return '127.0.0.1';
 }
 
-const HOST_TYPE = process.env.HOST_TYPE || 'localhost';
-let host;
-switch (HOST_TYPE) {
-  case 'ip':
-    host = getLocalIP();
-    break;
-  case 'localhost':
-    host = '127.0.0.1';
-    break;
-  case 'any':
-    host = '0.0.0.0';
-    break;
-  default:
-    host = '127.0.0.1';
-}
-
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
+
+let host;
+let fullAddress;
+
+if (NODE_ENV === 'production') {
+  host = 'g1-j1.store';
+  fullAddress = `https://${host}`;
+} else {
+  const HOST_TYPE = process.env.HOST_TYPE || 'localhost';
+  switch (HOST_TYPE) {
+    case 'ip':
+      host = getLocalIP();
+      break;
+    case 'localhost':
+      host = '127.0.0.1';
+      break;
+    case 'any':
+      host = '0.0.0.0';
+      break;
+    default:
+      host = '127.0.0.1';
+  }
+  fullAddress = `http://${host}:${PORT}`;
+}
 
 module.exports = {
   host,
-  port: PORT,
-  fullAddress: `http://${host}:${PORT}`,
+  port: NODE_ENV === 'production' ? undefined : PORT,
+  fullAddress,
 };
