32ad52114203dd1cebc6456b2dc7919cafe6086b
yarik.s <yarik.savelev.04@mail.ru>
2026-01-22 14:26:25 +0300

    update

--- src/routes/filesRoutes.js
+++ src/routes/filesRoutes.js
@@ -6,20 +6,19 @@ const authMiddleware = require("../middlewares/auth-middleware");
 const rateLimit = require("express-rate-limit");
 
 const uploadLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 минут
-  max: 20, // Максимальное количество запросов
+  windowMs: 15 * 60 * 1000, 
+  max: 20, 
   message: "Слишком много запросов на загрузку файлов. Попробуйте позже.",
   skipSuccessfulRequests: false,
   skip: (req, res) => {
-    // Пропускаем лимитер для пользователей с ролью admin
     return req.user && req.user.role === 'admin';
   }
 });
 
 router.post(
   "/upload",
-  authMiddleware(["all"]), // Сначала аутентификация
-  uploadLimiter,            // Потом лимитер (будет знать о роли)
+  authMiddleware(["all"]),
+  uploadLimiter,          
   multerMiddleware({
     fields: "files",
     maxFileSizeMB: 30,
